d439e82f0d11324aecd084f0dde8eaad6a4d739c
mohi14 <mohiuddinkhan1409@gmail.com>
2023-06-12 12:05:28 +0600

    some changes done

--- pages/signUpPage.js
+++ pages/signUpPage.js
@@ -1,18 +1,17 @@
-import React from 'react';
-import SignUp from '../components/Login/SignUp/SignUp';
-import Header from '../components/shared/Header';
-import SubFooter from '../components/shared/SubFooter';
+import React from "react";
+import SignUp from "../components/Login/SignUp/SignUp";
+import Header from "../components/shared/Header";
+import SubFooter from "../components/shared/SubFooter";
 
 const signUpPage = () => {
-    return (
-        <div>
-        <Header />
-        <div className="bg-[#EF4634] lg:bg-[#F5F5F5] lg:pt-[112px] px-[15px] py-[50px] lg:py-0 lg:px-0">
-          <SignUp />
-          <SubFooter style={{color:"black"}} />
-        </div>
+  return (
+    <div>
+      <div className="bg-[#EF4634] lg:bg-[#F5F5F5] lg:pt-[112px] px-[15px] py-[50px] lg:py-0 lg:px-0">
+        <SignUp />
+        <SubFooter style={{ color: "black" }} />
       </div>
-    );
+    </div>
+  );
 };
 
 export default signUpPage;
--- pages/voteNowPage.js
+++ pages/voteNowPage.js
@@ -1,18 +1,17 @@
-import React from 'react';
-import FooterCopy from '../components/shared/FooterCopy';
-import Header from '../components/shared/Header';
-import Voting from '../components/VoteNow/Voting';
+import React from "react";
+import FooterCopy from "../components/shared/FooterCopy";
+import Header from "../components/shared/Header";
+import Voting from "../components/VoteNow/Voting";
 
 const voteNowPage = () => {
-    return (
-        <div>
-            <Header/>
-            <div className='gradient_bg'>
-                <Voting/>
-                <FooterCopy/>
-            </div>
-        </div>
-    );
+  return (
+    <div>
+      <div className="gradient_bg">
+        <Voting />
+        <FooterCopy />
+      </div>
+    </div>
+  );
 };
 
 export default voteNowPage;
